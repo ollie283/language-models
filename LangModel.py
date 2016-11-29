21e16f0e187e1ebc9532165ca41b426b1c77a06b
Oliver Holland <odholl@essex.ac.uk>
2016-11-29 14:07:04 +0000

    Created a definiton that sorts the vocab file and appends to list with associated keys. Created a def that prints the unigram probs checking tomake sure the start and end sentence symbols are not taking into account. Finally created a main to run the program and at the moment, print the unigram probabilities

--- LangModel.py
+++ LangModel.py
@@ -25,20 +25,46 @@ class UnigramLanguageModel:
         self.unique_words = len(self.unigram_frequencies) - 2
         self.smoothing = smoothing
       
-def calculate_number_of_unigrams(sentences):
-    unigram_count = 0
-    for sentence in sentences:
-        # remove two for <s> and </s>
-        unigram_count += len(sentence) - 2
-    return unigram_count
+    def calculate_number_of_unigrams(sentences):
+        unigram_count = 0
+        for sentence in sentences:
+            # remove two for <s> and </s>
+            unigram_count += len(sentence) - 2
+        return unigram_count
 
-def calculate_unigram_probability(self, word):
-        word_probability_numerator = self.unigram_frequencies.get(word, 0)
-        word_probability_denominator = self.corpus_length
-        if self.smoothing:
-            word_probability_numerator += 1
-            # add one more to total number of seen unique words for UNK - unseen events
-            word_probability_denominator += self.unique_words + 1
-        print (float(word_probability_numerator) / float(word_probability_denominator))
+    def calculate_unigram_probability(self, word):
+            word_probability_numerator = self.unigram_frequencies.get(word, 0)
+            word_probability_denominator = self.corpus_length
+            if self.smoothing:
+                word_probability_numerator += 1
+                # add one more to total number of seen unique words for UNK - unseen events
+                word_probability_denominator += self.unique_words + 1
+            return float(word_probability_numerator) / float(word_probability_denominator)
 
-calculate_unigram_probability(self, word)
+    def sorted_vocabulary(self):
+        full_vocab = list(self.unigram_frequencies.keys())
+        full_vocab.remove(SENTENCE_START)
+        full_vocab.remove(SENTENCE_END)
+        full_vocab.sort()
+        full_vocab.append(UNK)
+        full_vocab.append(SENTENCE_START)
+        full_vocab.append(SENTENCE_END)
+        return full_vocab
+
+def print_unigram_probs(sorted_vocab_keys, model):
+    for vocab_key in sorted_vocab_keys:
+        if vocab_key != SENTENCE_START and vocab_key != SENTENCE_END:
+            print("{}: {}".format(vocab_key if vocab_key != UNK else "UNK",
+                                       model.calculate_unigram_probability(vocab_key)), end=" ")
+    print("")
+
+if __name__ == '__main__':
+    toy_dataset = read_sentences_from_file("./sampledata.txt")
+    toy_dataset_model_unsmoothed = UnigramLanguageModel(toy_dataset)
+
+    sorted_vocab_keys = toy_dataset_model_unsmoothed.sorted_vocabulary()
+
+    print("---------------- Toy dataset ---------------\n")
+    print("=== UNIGRAM MODEL ===")
+    print("- Unsmoothed  -")
+    print_unigram_probs(sorted_vocab_keys, toy_dataset_model_unsmoothed)
