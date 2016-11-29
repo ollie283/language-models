edf33b8fc2db9d1d8ef9639faa022f39932ba8eb
Oliver Holland <odholl@essex.ac.uk>
2016-11-29 13:24:30 +0000

    created UnigramLanguageModel class. Created a def to calculate number of unigrams (number of tokens in corpus)

--- LangModel.py
+++ LangModel.py
@@ -8,6 +8,28 @@ SENTENCE_END = "</s>"
 
 def read_sentences_from_file():
     with open("./sampledata.txt", "r") as f:
-        print ([re.split("\s+", line.rstrip('\n')) for line in f])
+        return [re.split("\s+", line.rstrip('\n')) for line in f]
 
-read_sentences_from_file()
+
+    
+class UnigramLanguageModel:
+    def __init__(self, sentences, smoothing=False):
+        self.unigram_frequencies = dict()
+        self.corpus_length = 0
+        for sentence in sentences:
+            for word in sentence:
+                self.unigram_frequencies[word] = self.unigram_frequencies.get(word, 0) + 1
+                if word != SENTENCE_START and word != SENTENCE_END:
+                    self.corpus_length += 1
+        # subtract 2 because unigram_frequencies dictionary contains values for SENTENCE_START and SENTENCE_END
+        self.unique_words = len(self.unigram_frequencies) - 2
+        self.smoothing = smoothing
+      
+def calculate_number_of_unigrams(sentences):
+    unigram_count = 0
+    for sentence in sentences:
+        # remove two for <s> and </s>
+        unigram_count += len(sentence) - 2
+    print (unigram_count)
+
+calculate_number_of_unigrams(read_sentences_from_file())
